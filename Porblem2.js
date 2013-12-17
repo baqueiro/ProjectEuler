e0ca299d0e1f00d5aa1f828d66d83528ff7aaa45
Guillermo <baqueiro@gmail.com>
2013-12-16 19:07:01 -0600

    ya, ahora a ponerle timers

--- Porblem2.js
+++ Porblem2.js
@@ -1,22 +1,17 @@
 //ver 2:
 var a1 = 1, a2=2, a3=0, sum = 2;  
 
-while(a3 <= 4000000){  
-  
- // 1
- // 1
- // 2 = 1+1
- // 3=2+1
- // 5=3+2
- // 8=5+3=(3+2)+(2+1)
-  
-  // 8 =  ( (a2+a1) + a2)+(a2+a1) = 3*a2 + 2*a1
-  
+while(true){  
+
   a3 = 3*a2 + 2*a1;
+  
+  if(a3 > 4000000) break;
   
   a1 = 2*a2+a1;
   
   a2 = a3;
+  
+  console.log(a3);
   
   sum += a3;
   
@@ -28,5 +23,5 @@ console.log(sum);
 
 // ver 1:
 var a1 = 1, a2=2, a3=0, sum = 2;  
-while(a3 <= 4000000){ a3=a1+a2; a1=a2; a2=a3; if(a3%2==0) { sum += a3; } }; 
+while(a3 <= 4000000){ a3=a1+a2; a1=a2; a2=a3; if(a3%2==0) { sum += a3; console.log(a3); } }; 
 console.log(sum);
