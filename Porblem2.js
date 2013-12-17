a086e4f11f589fe3dbd8a58f41fb734d53c8ccd6
Guillermo <baqueiro@gmail.com>
2013-12-16 19:27:47 -0600

    comentarios

--- Porblem2.js
+++ Porblem2.js
@@ -1,7 +1,6 @@
 var n = 4000000;
 
 // ver 1:
-
 var start = new Date().getTime();
 var a1 = 1, a2=2, a3=0, sum = 2;  
 while(a3 <= n){ a3=a1+a2; a1=a2; a2=a3; if(a3%2==0) { sum += a3; } }; 
@@ -11,7 +10,7 @@ console.log(sum);
 console.log("time ver1:");
 console.log(endT-start);
 
-//ver 2:
+//ver 2: de fibonacci par en fibonacci par
 start = new Date().getTime();
 var a1 = 1, a2=2, a3=0, sum = 2;  
 while(true){  
