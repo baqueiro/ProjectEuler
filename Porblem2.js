9929de527200fc1f8a105fae4203075c4ebcbb53
Guillermo <baqueiro@gmail.com>
2013-12-16 19:26:59 -0600

    Quitando logs dentro de los ciclos
    No podría saber desde javascript porque hasta ahora mi unidad más pequeña son los milisegundos y no se si pueda tener décimas

--- Porblem2.js
+++ Porblem2.js
@@ -4,7 +4,7 @@ var n = 4000000;
 
 var start = new Date().getTime();
 var a1 = 1, a2=2, a3=0, sum = 2;  
-while(a3 <= n){ a3=a1+a2; a1=a2; a2=a3; if(a3%2==0) { sum += a3; console.log(a3); } }; 
+while(a3 <= n){ a3=a1+a2; a1=a2; a2=a3; if(a3%2==0) { sum += a3; } }; 
 var endT = new Date().getTime();
 console.log("sum:");
 console.log(sum);
@@ -19,7 +19,6 @@ while(true){
   if(a3 > n) break;
   a1 = 2*a2+a1;
   a2 = a3;
-  console.log(a3);
   sum += a3;
 };
 var endT = new Date().getTime();
