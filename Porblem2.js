39f766b26a2eb02ecb4fe02df0dd0bd85ef946e1
Guillermo <baqueiro@gmail.com>
2013-12-16 19:24:18 -0600

    ya, de las dos formas
    Es muy difícil ver alguna mejora en javascript para este problema

--- Porblem2.js
+++ Porblem2.js
@@ -1,27 +1,31 @@
-//ver 2:
-var a1 = 1, a2=2, a3=0, sum = 2;  
-
-while(true){  
-
-  a3 = 3*a2 + 2*a1;
-  
-  if(a3 > 4000000) break;
-  
-  a1 = 2*a2+a1;
-  
-  a2 = a3;
-  
-  console.log(a3);
-  
-  sum += a3;
-  
-}; 
-
-console.log(sum);
-
-
+var n = 4000000;
 
 // ver 1:
+
+var start = new Date().getTime();
 var a1 = 1, a2=2, a3=0, sum = 2;  
-while(a3 <= 4000000){ a3=a1+a2; a1=a2; a2=a3; if(a3%2==0) { sum += a3; console.log(a3); } }; 
+while(a3 <= n){ a3=a1+a2; a1=a2; a2=a3; if(a3%2==0) { sum += a3; console.log(a3); } }; 
+var endT = new Date().getTime();
+console.log("sum:");
 console.log(sum);
+console.log("time ver1:");
+console.log(endT-start);
+
+//ver 2:
+start = new Date().getTime();
+var a1 = 1, a2=2, a3=0, sum = 2;  
+while(true){  
+  a3 = 3*a2 + 2*a1;
+  if(a3 > n) break;
+  a1 = 2*a2+a1;
+  a2 = a3;
+  console.log(a3);
+  sum += a3;
+};
+var endT = new Date().getTime();
+console.log("sum:");
+console.log(sum);
+console.log("time ver2:");
+console.log(endT-start);
+
+
